02799ca3a3258e938cc4e2cbf4a3fb3dd4160943
Nikki <nikki.asteinza@gmail.com>
2023-03-28 18:22:36 +0200

    Update http_handlers.go

--- service/http_handlers.go
+++ service/http_handlers.go
@@ -10,6 +10,7 @@ import (
 )
 
 func (s *Service) getRtcToken(c *gin.Context) {
+	enableCors(&w)
 	log.Println("Generating RTC token")
 	// get param values
 	channelName, tokenType, uidStr, _, role, expireTimestamp, err := s.parseRtcParams(c)
